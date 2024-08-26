e0f3e969bfa4ed87066e0405c1c933d9aab63b01
parthparmar-addshoppers <parth@addshoppers.com>
2024-08-26 12:15:11 -0400

    test 2
    test 2

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 console.log('index.js');
 
 var url = 'https://parthparmar-addshoppers.github.io/interstitial_tab_test/index2.html';
-window.open(url);
+// window.open(url);
 // window.location.replace('https://chewy.com/', 'landing_page_url_safeopt');
 
 function test_function() {
     console.log('test_function');
     window.open(url);
 }
+
+function run_open() {
+    console.log('run_open');
+    document.querySelector('.landing_page').click();
+};
+
+run_open();
